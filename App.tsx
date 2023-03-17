911e31667f56a24eca9c4f93a532d5ebb05f7a8a
Allan Juan <allanjuansil@gmail.com>
2023-03-17 16:26:46 -0300

    Criar estrutura básica

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <View style={styles.topBar}>
+        <Text style={styles.topBarText}>My Health</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+  },
+  topBar: {
+    paddingVertical: 24,
+    width: "100%",
+    shadowColor: "#171717",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  topBarText: {
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "bold",
   },
 });

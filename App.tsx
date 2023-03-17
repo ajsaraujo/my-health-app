2ae087de8ded9b1211512772e7bac05357c5a65c
Allan Juan <allanjuansil@gmail.com>
2023-03-17 17:50:13 -0300

    Configurar navegação com React Navigation

--- App.tsx
+++ App.tsx
@@ -1,85 +1,27 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./src/pages/Home";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const modules = ["Calculadoras", "Códigos", "Diário", "Medicamentos"];
-
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-
-      <View style={styles.topBar}>
-        <Text style={styles.topBarText}>My Health</Text>
-      </View>
-
-      <View style={styles.contentContainer}>
-        <Text style={styles.helloText}>Olá, Fulano.</Text>
-        <Text style={styles.letsTakeCareText}>
-          Vamos cuidar da sua sáude hoje? ❤️
-        </Text>
-
-        <View style={styles.modulesContainer}>
-          {modules.map((moduleName) => (
-            <View style={styles.moduleCard}>
-              <Text>{moduleName}</Text>
-            </View>
-          ))}
-        </View>
-      </View>
-    </View>
+    <NavigationContainer
+      theme={{
+        ...DefaultTheme,
+        colors: {
+          ...DefaultTheme.colors,
+          background: "white",
+        },
+      }}
+    >
+      <Stack.Navigator initialRouteName="My Health">
+        <Stack.Screen
+          name="My Health"
+          component={Home}
+          options={{ headerTitleAlign: "center" }}
+        ></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-  },
-  topBar: {
-    paddingVertical: 24,
-    width: "100%",
-    shadowColor: "#171717",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-  },
-  topBarText: {
-    textAlign: "center",
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  contentContainer: {
-    paddingHorizontal: 24,
-    paddingVertical: 24,
-  },
-  helloText: {
-    fontSize: 20,
-    marginBottom: 8,
-  },
-  letsTakeCareText: {
-    fontSize: 16,
-    color: "#212121",
-  },
-  modulesContainer: {
-    marginTop: 32,
-    justifyContent: "center",
-    display: "flex",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    columnGap: 24,
-    rowGap: 24,
-  },
-  moduleCard: {
-    width: 108,
-    height: 108,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-
-    borderRadius: 5,
-
-    shadowColor: "#171717",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.15,
-    shadowRadius: 6,
-  },
-});
---
+++ src/pages/Home.tsx
@@ -0,0 +1,78 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Home() {
+  const modules = ["Calculadoras", "Códigos", "Diário", "Medicamentos"];
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.contentContainer}>
+        <Text style={styles.helloText}>Olá, Fulano.</Text>
+        <Text style={styles.letsTakeCareText}>
+          Vamos cuidar da sua saúde hoje? ❤️
+        </Text>
+
+        <View style={styles.modulesContainer}>
+          {modules.map((moduleName) => (
+            <View style={styles.moduleCard}>
+              <Text>{moduleName}</Text>
+            </View>
+          ))}
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+  },
+  topBar: {
+    paddingVertical: 24,
+    width: "100%",
+    shadowColor: "#171717",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  topBarText: {
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  contentContainer: {
+    paddingHorizontal: 24,
+    paddingVertical: 24,
+  },
+  helloText: {
+    fontSize: 20,
+    marginBottom: 8,
+  },
+  letsTakeCareText: {
+    fontSize: 16,
+    color: "#212121",
+  },
+  modulesContainer: {
+    marginTop: 32,
+    justifyContent: "center",
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    columnGap: 24,
+    rowGap: 24,
+  },
+  moduleCard: {
+    width: 108,
+    height: 108,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+
+    borderRadius: 5,
+
+    shadowColor: "#171717",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.15,
+    shadowRadius: 6,
+  },
+});

f55d00beff989b4477d729464d891ddd5d2eb1a7
Allan Juan <allanjuansil@gmail.com>
2023-03-18 11:14:11 -0300

    Integrar ESLint e Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,21 +3,20 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
   overrides: [],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
-    indent: ["error", "spaces"],
-    "linebreak-style": "off",
-    quotes: ["error", "single"],
-    semi: ["error", "never"],
+    indent: ['error', 'spaces'],
+    'linebreak-style': 'off',
   },
-};
+}

5d1074e042a2a18ef774a6cce9da00e47a568407
Carlos Henrique Santos Silveira <109424074+CarlosDcomp@users.noreply.github.com>
2023-03-25 16:32:51 -0300

    First commit

--- src/modules/remedies/Remedies.tsx
+++ src/modules/remedies/Remedies.tsx
@@ -1,8 +1,12 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import { MyHealthModule } from '../../modules'
 import { RouteParams } from '../../routeParams'
-import { globalStyles } from '../../shared/ui/globalStyles'
+import { styles } from './css/styles'
+
+const setaEsquerda = require('./img/chevron-left.png')
+const setaDireita = require('./img/chevron-right.png')
+const setaInfo = require('./img/arrow-info.png')
 
 type RemediesProps = NativeStackScreenProps<
   RouteParams,
@@ -11,8 +15,66 @@ type RemediesProps = NativeStackScreenProps<
 
 export default function Remedies(props: RemediesProps) {
   return (
-    <View style={globalStyles.defaultContainer}>
-      <Text>Hello, world</Text>
+    <View style={styles.containerRemedies}>
+      {/* Tab Navigation */}
+      <View style={styles.navigationRemediesContainer}>
+        {/* @todo fazer botao */}
+        <Image source={setaEsquerda} style={styles.iconNavigationLeft}></Image>
+        <Image source={setaDireita} style={styles.iconNavigationRight}></Image>
+      </View>
+
+      {/* Tab Title */}
+      <View style={styles.titleHomeContainer}>
+        {/* substituir dps pelo username do user e date atual */}
+        <Text style={styles.helloRemediesText}>Olá, José!</Text>
+        <Text style={styles.helloRemediesDate}>19/09/2022</Text>
+      </View>
+
+      {/* Tab Calendar */}
+      <View style={styles.containerDay}>
+        <Text style={[styles.tagDay, { opacity: 0.5 }]}>seg</Text>
+        <Text style={[styles.tagDay, { opacity: 0.5 }]}>ter</Text>
+        <Text style={[styles.tagDay, { opacity: 0.5 }]}>qua</Text>
+        <Text style={[styles.tagDay, { opacity: 0.5 }]}>qui</Text>
+        <Text style={[styles.tagDay, { opacity: 0.5 }]}>sex</Text>
+        <Text style={[styles.tagDay, { opacity: 0.5 }]}>sáb</Text>
+        <Text style={[styles.tagDay, { opacity: 0.5 }]}>dom</Text>
+      </View>
+
+      {/* Tab Calendar content @todo substituir com info do DB */}
+      <View>
+        <View style={styles.containerInfoDay}>
+          <Text style={styles.infoDayHour}>8:00</Text>
+          <Text style={styles.infoDayText}>Tomar insulina</Text>
+          <TouchableOpacity style={styles.infoDayButton}>
+            <Image source={setaInfo} style={styles.infoDayButtonText}></Image>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.containerInfoDay}>
+          <Text style={styles.infoDayHour}>15:00</Text>
+          <Text style={styles.infoDayText}>Exame de Rotina</Text>
+          <TouchableOpacity style={styles.infoDayButton}>
+            <Image source={setaInfo} style={styles.infoDayButtonText}></Image>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.containerInfoDay}>
+          <Text style={styles.infoDayHour}>21:30</Text>
+          <Text style={styles.infoDayText}>Exame Urinário</Text>
+          <TouchableOpacity style={styles.infoDayButton}>
+            <Image source={setaInfo} style={styles.infoDayButtonText}></Image>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {/* Tab Actions Buttons */}
+
+      <View style={styles.containerSchedulingButton}>
+        <TouchableOpacity style={styles.schedulingButton}>
+          <Text style={styles.schedulingButtonText}>Novo Agendamento</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }

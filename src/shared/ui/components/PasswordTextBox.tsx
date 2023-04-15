8934b1ed9ee2dbde716102eb4bde2f5981d697e6
Allan Juan <allanjuansil@gmail.com>
2023-04-15 15:18:16 -0300

    Esconder e exibir senha

--- src/shared/ui/components/PasswordTextBox.tsx
+++ src/shared/ui/components/PasswordTextBox.tsx
@@ -1,5 +1,45 @@
+import { Pressable, View } from 'react-native'
 import { TextBox, TextBoxProps } from './TextBox'
+import Ionicons from '@expo/vector-icons/Ionicons'
+import { useState } from 'react'
 
 export function PasswordTextBox(props: TextBoxProps) {
-  return <TextBox secureTextEntry={true}></TextBox>
+  const [passwordIsVisible, setPasswordVisibility] = useState(false)
+
+  function togglePasswordVisibility() {
+    setPasswordVisibility(!passwordIsVisible)
+  }
+
+  return (
+    <View>
+      <TextBox secureTextEntry={!passwordIsVisible}></TextBox>
+      <PasswordVisibilityButton
+        passwordIsVisible={passwordIsVisible}
+        onPress={togglePasswordVisibility}
+      ></PasswordVisibilityButton>
+    </View>
+  )
+}
+
+function PasswordVisibilityButton(props: {
+  passwordIsVisible: boolean
+  onPress: () => void
+}) {
+  return (
+    <Pressable
+      onPress={props.onPress}
+      style={{
+        position: 'absolute',
+        right: 8,
+        justifyContent: 'center',
+        flexDirection: 'column',
+        height: '100%',
+      }}
+    >
+      <Ionicons
+        size={24}
+        name={props.passwordIsVisible ? 'eye' : 'eye-off'}
+      ></Ionicons>
+    </Pressable>
+  )
 }

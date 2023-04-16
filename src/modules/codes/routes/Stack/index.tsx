d10023534e04590aababb60d410874dfd9118b30
Allan Juan <allanjuansil@gmail.com>
2023-04-16 17:47:42 -0300

    Renomear consulta e home, e movê-las para baixo de pages

--- src/modules/codes/routes/Stack/index.tsx
+++ src/modules/codes/routes/Stack/index.tsx
@@ -1,19 +1,21 @@
-import React from "react";
-import Home from "../../screens/Home"
-import Consulta from "../../screens/Consulta";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { propsNavigationStack } from "./Models";
-
+import React from 'react'
+import Home from '../../pages/Home'
+import Consulta from '../../pages/Consulta'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { propsNavigationStack } from './Models'
 
 const { Navigator, Screen } = createNativeStackNavigator<propsNavigationStack>()
 
 export default function () {
-    return (
-        <Navigator initialRouteName="Home" screenOptions={{
-            headerShown:false,
-            }}>
-            <Screen name = "Home" component= {Home} />
-            <Screen name = "Consulta" component= {Consulta} />
-        </Navigator>
-    )
+  return (
+    <Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Screen name="Home" component={Home} />
+      <Screen name="Consulta" component={Consulta} />
+    </Navigator>
+  )
 }

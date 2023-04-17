4e03c5396e7f5d96ce6476096086304618d264ca
Allan Juan <allanjuansil@gmail.com>
2023-04-17 17:24:33 -0300

    Configurar path mappings

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: '.',
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          alias: {
+            '@shared': './src/shared',
+          },
+        },
+      ],
+    ],
+  }
+}

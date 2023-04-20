6b9a01da26d095f9cad046ec54e3705549c28462
Allan Juan <allanjuansil@gmail.com>
2023-04-20 15:36:29 -0300

    Corrigir testes de SelectButton

--- src/shared/ui/components/SelectButton.test.tsx
+++ src/shared/ui/components/SelectButton.test.tsx
@@ -5,26 +5,43 @@ import { useState } from 'react'
 import { fireEvent, render, screen } from '@testing-library/react-native'
 
 describe('SelectButton', () => {
+  function ExampleComponent() {
+    const options = ['Dia', 'Tarde', 'Noite']
+    const [selectedOption, setSelectedOption] = useState('')
+
+    return (
+      <View>
+        <SelectButton
+          options={options}
+          selectedOption={selectedOption}
+          onSelectionChange={setSelectedOption}
+        ></SelectButton>
+
+        <Text>Opção selecionada: {selectedOption}</Text>
+      </View>
+    )
+  }
   it('deve selecionar uma opção', () => {
-    function ExampleComponent() {
-      const options = ['Dia', 'Tarde', 'Noite']
-      const [selectedOption, setSelectedOption] = useState('')
-
-      return (
-        <View>
-          <SelectButton
-            options={options}
-            selectedOption={selectedOption}
-            onSelectionChange={setSelectedOption}
-          ></SelectButton>
-
-          <Text>Opção selecionada: {selectedOption}</Text>
-        </View>
-      )
-    }
-
     render(<ExampleComponent></ExampleComponent>)
 
+    fireEvent.press(screen.getByText('Tarde'))
+
+    expect(screen.queryByText('Opção selecionada: Tarde')).toBeVisible()
+  })
+
+  it('deve alterar a seleção', () => {
+    render(<ExampleComponent></ExampleComponent>)
+
+    fireEvent.press(screen.getByText('Tarde'))
+    fireEvent.press(screen.getByText('Noite'))
+
+    expect(screen.queryByText('Opção selecionada: Noite')).toBeVisible()
+  })
+
+  it('não deve desfazer a seleção se clicar duas vezes', () => {
+    render(<ExampleComponent></ExampleComponent>)
+
+    fireEvent.press(screen.getByText('Tarde'))
     fireEvent.press(screen.getByText('Tarde'))
 
     expect(screen.queryByText('Opção selecionada: Tarde')).toBeVisible()

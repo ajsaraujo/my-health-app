c88a9b26aaff6b55b82829b8dee01d9a5c2eb86a
Marcel <marcel.santos017@gmail.com>
2023-04-28 22:18:57 -0300

    add medicamentos asyncStorage /register

--- src/modules/medicines/views/form/register/index.jsx
+++ src/modules/medicines/views/form/register/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import {
   Modal,
   View,
@@ -11,11 +12,43 @@ import {
 import { styles } from '../../../css/info/schedule'
 
 const RegisterModalMed = ({ visible, onClose }) => {
-  const handleSubmit = () => {
-    // Lógica para agendar o evento aqui
-    onClose()
+  const [nomeMed, setNomeMed] = useState('')
+  const [funcMed, setFuncMed] = useState('')
+  const [descMed, setDescMed] = useState('')
+
+  const medicamentos = {
+    nomeMedicamento: nomeMed,
+    funcMedicamento: funcMed,
+    descMedicamento: descMed,
+  }
+  const chave = 'meusMedicamentos'
+
+  //adiciona um novo medicamento ao asyncStorage
+  async function handleSubmit() {
+    const jsonMedicamentos = JSON.stringify(medicamentos)
+
+    try {
+      await AsyncStorage.setItem(chave, jsonMedicamentos)
+      console.log('Medicamentos salvos com sucesso!')
+      console.log(medicamentos)
+    } catch (error) {
+      console.log('Erro ao salvar medicamentos:', error)
+    }
   }
 
+  //retorna o medicamento cadastrado
+  async function getAllMed() {
+    try {
+      const value = await AsyncStorage.getItem(chave)
+      if (value !== null) {
+        console.log('Remedios cadastrados:', value) // valor encontrado
+      } else {
+        console.log('Nenhum valor encontrado para a chave ')
+      }
+    } catch (error) {
+      console.log(error) // erro ao recuperar valor
+    }
+  }
   return (
     <Modal
       visible={visible}
@@ -27,9 +60,32 @@ const RegisterModalMed = ({ visible, onClose }) => {
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
             <Text style={styles.modalTitle}>Cadastrar Medicamento</Text>
-            <TextInput style={styles.input} placeholder="Nome" />
-            <TextInput style={styles.input} placeholder="Função" />
-            <TextInput style={styles.input} placeholder="Descrição" />
+            <TextInput
+              onChangeText={(event) => {
+                setNomeMed(event)
+              }}
+              value={nomeMed}
+              style={styles.input}
+              placeholder="Nome"
+            />
+
+            <TextInput
+              onChangeText={(event) => {
+                setFuncMed(event)
+              }}
+              value={funcMed}
+              style={styles.input}
+              placeholder="Função"
+            />
+
+            <TextInput
+              onChangeText={(event) => {
+                setDescMed(event)
+              }}
+              value={descMed}
+              style={styles.input}
+              placeholder="Descrição"
+            />
 
             <View style={styles.containerSchedulingButton}>
               <TouchableOpacity
@@ -45,5 +101,4 @@ const RegisterModalMed = ({ visible, onClose }) => {
     </Modal>
   )
 }
-
 export default RegisterModalMed

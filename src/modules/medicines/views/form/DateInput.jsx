404307e0cb48431c8398f37c9dc225d1210d9de1
Myngruan <77254649+Myngruan@users.noreply.github.com>
2023-05-01 18:03:38 -0300

    DateInput.jsx

--- src/modules/medicines/views/form/DateInput.jsx
+++ src/modules/medicines/views/form/DateInput.jsx
@@ -2,16 +2,45 @@ import React, { useState } from 'react'
 import { TextInput } from 'react-native'
 
 const DateInput = (props) => {
+  const [mesAtual, setMesAtual] = useState(new Date().getMonth() + 1)
+  const [anoAtual, setAnoAtual] = useState(new Date().getFullYear())
+
+  function AnoBissexto(year) {
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
+  }
+
+  function diaspormes(year, month) {
+    if (month === 2) {
+      return AnoBissexto(year) ? 29 : 28
+    } else if ([4, 6, 9, 11].includes(month)) {
+      return 30
+    } else {
+      return 31
+    }
+  }
+
   const handleTextChange = (text) => {
     // Remove caracteres que não são números
     const cleanedValue = text.replace(/[^0-9]/g, '')
 
     // Verifica se o valor segue o formato dd/mm/aaaa
-    if (cleanedValue.length <= 2) {
+    if (
+      cleanedValue.length <= 2 &&
+      Number(cleanedValue) <= diaspormes(anoAtual, mesAtual)
+    ) {
       props.setValue(cleanedValue)
-    } else if (cleanedValue.length <= 4) {
+    } else if (
+      cleanedValue.length <= 4 &&
+      Number(cleanedValue.slice(0, 2)) <= diaspormes(anoAtual, mesAtual) &&
+      Number(cleanedValue.slice(2, 4)) <= mesAtual
+    ) {
       props.setValue(`${cleanedValue.slice(0, 2)}/${cleanedValue.slice(2)}`)
-    } else if (cleanedValue.length <= 8) {
+    } else if (
+      cleanedValue.length <= 8 &&
+      Number(cleanedValue.slice(0, 2)) <= diaspormes(anoAtual, mesAtual) &&
+      Number(cleanedValue.slice(2, 4)) <= 12 &&
+      Number(cleanedValue.slice(4)) <= anoAtual
+    ) {
       props.setValue(
         `${cleanedValue.slice(0, 2)}/${cleanedValue.slice(
           2,
--- src/modules/medicines/views/form/TimeInput.jsx
+++ src/modules/medicines/views/form/TimeInput.jsx
@@ -10,7 +10,13 @@ const TimeInput = (props) => {
     if (cleanedValue.length <= 2) {
       props.setValue(cleanedValue)
     } else {
-      props.setValue(`${cleanedValue.slice(0, 2)}:${cleanedValue.slice(2)}`)
+      const horas = parseInt(cleanedValue.slice(0, 2))
+      const minutos = parseInt(cleanedValue.slice(2))
+
+      // Verifica se as horas estão dentro do intervalo de 0 a 23
+      if (horas >= 0 && horas <= 23 && minutos >= 0 && minutos <= 59) {
+        props.setValue(`${cleanedValue.slice(0, 2)}:${cleanedValue.slice(2)}`)
+      }
     }
   }
 

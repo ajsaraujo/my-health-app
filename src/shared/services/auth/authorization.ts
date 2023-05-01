91b7af207a32c58f8454e419d1b7a3d15e140390
Allan Juan <allanjuansil@gmail.com>
2023-05-01 11:29:35 -0300

    Criar métodos para verificar autorização

---
+++ src/shared/services/auth/authorization.ts
@@ -0,0 +1,15 @@
+import { getSession } from './session'
+
+export async function hasPermission(permission: string): Promise<boolean> {
+  const session = await getSession()
+
+  if (!session) {
+    return false
+  }
+
+  return session.permissions.includes(permission)
+}
+
+export async function isAdmin() {
+  return hasPermission('adm')
+}
